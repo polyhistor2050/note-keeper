2b072df24e785eed7089b43df3a16e832b0c31c2
polyhistor2050 <chrisspaul2050@gmail.com>
2023-08-07 23:55:29 +0300

    implement the add note functionality

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+import CreateArea from "./CreateArea";
+
+function createNotes(noteItem){
+  return <Note 
+    key={noteItem.key}
+    title={noteItem.title}
+    content={noteItem.content}
+  />
+}
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note){
+    setNotes(prevNotes => {
+      return [...prevNotes, note]
+    })
+  }
+
   return (
     <div>
-      <Header />
-      <Note />
+      <Header /> 
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem) => {
+        return <Note
+          title={noteItem.title}
+          content={noteItem.content} />
+      })}
       <Footer />
     </div>
   );
---
+++ src/notes.js
@@ -0,0 +1,34 @@
+const notes  = [
+    {
+        key: 1,
+        title: "Delegation",
+        content: 
+        "Q. How many programmer does it take to change the light bulb ? "
+    },
+    {
+        key: 2,
+        title: "Loops",
+        content:
+        "How to keep programmer in shower forever. Show him shame"
+    },
+    {
+        key: 3,
+        title: "Arrays",
+        content:
+        "Q. Why did the programmer quit his job? Because he didn't get"
+    },
+    {
+        key: 4,
+        title: "Hardware vs. Software",
+        content:
+        "What's the difference between hardware and Software"
+    },
+    {
+        key: 5,
+        title: "Big Ideas",
+        content:
+        "Eat more sushi"
+    }
+]
+
+export default notes;
